ee2dd3ac440e000ad2e6aab34cb1d3d289e98935
Caio Guedes <caioguedesam@gmail.com>
2022-05-02 16:55:48 -0300

    some boilerplate

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <glad/glad.h>
 #include <SDL.h>
+#include <SDL_opengl.h>
 
 int main(int argc, char* argv[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_GL_LoadLibrary(NULL);
+	SDL_Window* window = SDL_CreateWindow(
+		"test!",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		640, 480, SDL_WINDOW_OPENGL
+	);
+	SDL_GLContext maincontext = SDL_GL_CreateContext(window);
+
+	if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) {
+		printf("failed to initialize GLAD\n");
+		return 0;
+	}
+
 	printf("Hello world!");
+	while (true)
+	{
+		printf(".");
+	}
 	return 0;
 }
