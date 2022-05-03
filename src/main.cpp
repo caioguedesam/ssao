6d697ad86c4a456b1f72defb7b4dfdb5d5c54b55
Caio Guedes <caioguedesam@gmail.com>
2022-05-03 19:36:11 -0300

    todo fix fragments

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <glm/glm.hpp>
 #include <SDL.h>
 #include <vector>
+#include <iostream>
 
 #if _DEBUG
 #include <cassert>
@@ -14,16 +15,14 @@
 #define ASSERT_ZERO(EXPR, MSG) do {} while(false)
 #endif
 
-#define UINT32_MAX (uint32_t)-1
-
 // Default resources
 // Vertices for 1x1 quad centered on origin
 float g_quadVertices[] =
 {
-	-0.5f, -0.5f, 0.0f,
-	-0.5f, 0.5f, 0.0f,
-	 0.5f, 0.5f, 0.0f,
-	 0.5f,  -0.5f, 0.0f
+		0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left
 };
 // Indices for 1x1 quad
 uint32_t g_quadIndices[] =
@@ -31,6 +30,20 @@ uint32_t g_quadIndices[] =
 	0, 2, 1,
 	0, 3, 2
 };
+// Default vertex shader source code
+const char* g_vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(0.0f, 0.0f, 0.0f, 1.0);\n"
+"}\0";
+// Default fragment shader source code
+const char* g_fragmentShaderSource = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"   color = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+"}\0";
 
 class Buffer
 {
@@ -43,26 +56,90 @@ public:
 	Buffer()
 	{
 		glGenBuffers(1, &handle);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
 	}
 
 	void Bind(GLenum bindTarget)
 	{
 		glBindBuffer(bindTarget, handle);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
 	}
 
-	void SetData(GLenum bindTarget, uint32_t bufferSize, uint32_t bufferCount, void* bufferData)
+	void Init(GLenum bindTarget, uint32_t bufferSize, uint32_t bufferCount, void* bufferData)
 	{
 		sizeInBytes = bufferSize;
 		count = bufferCount;
 		pData = bufferData;
 		Bind(bindTarget);
 		glBufferData(bindTarget, sizeInBytes, pData, GL_STATIC_DRAW);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
 	}
+};
 
-	/*void Init()
+class Shader
+{
+public:
+	uint32_t programHandle = UINT32_MAX;
+
+	uint32_t vertexHandle = UINT32_MAX;
+	uint32_t fragmentHandle = UINT32_MAX;
+
+	const char* vertexSrc = nullptr;
+	const char* fragmentSrc = nullptr;
+
+	void InitAndCompile(const char* vSrc, const char* fSrc)
 	{
-		glGenBuffers(1, &handle);
-	}*/
+		if (programHandle != UINT32_MAX)
+		{
+			glDeleteProgram(programHandle);
+		}
+		programHandle = glCreateProgram();
+		if (!vertexSrc || strcmp(vertexSrc, vSrc) != 0)
+		{
+			vertexSrc = vSrc;
+			if (vertexHandle == UINT32_MAX)
+			{
+				vertexHandle = glCreateShader(GL_VERTEX_SHADER);
+			}
+			glShaderSource(vertexHandle, 1, &vertexSrc, NULL);
+			glCompileShader(vertexHandle);
+			int  success;
+			char infoLog[512];
+			glGetShaderInfoLog(vertexHandle, 512, NULL, infoLog);
+			glGetShaderiv(vertexHandle, GL_COMPILE_STATUS, &success);
+			if (!success) {
+				std::cout << "ERROR::SHADER::COMPILE_FAILED\n" << infoLog << std::endl;
+			}
+		}
+		if (!fragmentSrc || strcmp(fragmentSrc, fSrc) != 0)
+		{
+			fragmentSrc = fSrc;
+			if (fragmentHandle == UINT32_MAX)
+			{
+				fragmentHandle = glCreateShader(GL_FRAGMENT_SHADER);
+			}
+			glShaderSource(fragmentHandle, 1, &fragmentSrc, NULL);
+			glCompileShader(fragmentHandle);
+			int  success;
+			char infoLog[512];
+			glGetShaderInfoLog(vertexHandle, 512, NULL, infoLog);
+			ASSERT(glGetError() == GL_NO_ERROR, "");
+			glGetShaderiv(fragmentHandle, GL_COMPILE_STATUS, &success);
+			if (!success) {
+				std::cout << "ERROR::SHADER::COMPILE_FAILED\n" << infoLog << std::endl;
+			}
+		}
+		glAttachShader(programHandle, vertexHandle);
+		glAttachShader(programHandle, fragmentHandle);
+		glLinkProgram(programHandle);
+		int  success;
+		char infoLog[512];
+		glGetProgramiv(programHandle, GL_LINK_STATUS, &success);
+		if (!success) {
+			glGetProgramInfoLog(programHandle, 512, NULL, infoLog);
+			std::cout << "ERROR::SHADER::LINK_FAILED\n" << infoLog << std::endl;
+		}
+	}
 };
 
 void InitSDL()
@@ -119,6 +196,55 @@ public:
 	}
 };
 
+class Renderable
+{
+public:
+	uint32_t vaoHandle = UINT32_MAX;
+	Buffer* vertexBuffer;
+	Buffer* indexBuffer;
+	Shader* shader;
+
+	void SetVertexData(Buffer* vb, Buffer* ib)
+	{
+		if (vaoHandle == UINT32_MAX)
+		{
+			glGenVertexArrays(1, &vaoHandle);
+			ASSERT(glGetError() == GL_NO_ERROR, "");
+		}
+
+		vertexBuffer = vb;
+		indexBuffer = ib;
+
+		glBindVertexArray(vaoHandle);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
+		vertexBuffer->Bind(GL_ARRAY_BUFFER);
+		indexBuffer->Bind(GL_ELEMENT_ARRAY_BUFFER);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * vertexBuffer->sizeInBytes / vertexBuffer->count, (void*)0);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
+		glEnableVertexAttribArray(0);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
+
+		glBindVertexArray(0);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
+	}
+
+	void SetShader(Shader* sh)
+	{
+		shader = sh;
+	}
+
+	void Draw()
+	{
+		glUseProgram(shader->programHandle);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
+		glBindVertexArray(vaoHandle);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
+		glDrawElements(GL_TRIANGLES, indexBuffer->count, GL_UNSIGNED_INT, 0);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
+	}
+};
+
 class Renderer
 {
 public:
@@ -148,6 +274,7 @@ public:
 		ASSERT(pWindow, "Trying to create new OpenGL context without window associated to renderer.");
 		pGlContextHandle = SDL_GL_CreateContext(pWindow->handle);
 		ASSERT(pGlContextHandle, "Failed to create new OpenGL context.");
+		SDL_GL_MakeCurrent(pWindow->handle, pGlContextHandle);
 	}
 
 	void RetrieveAPIFunctionLocations()
@@ -159,12 +286,13 @@ public:
 	void SetViewport(uint32_t width, uint32_t height, uint32_t x, uint32_t y)
 	{
 		glViewport(x, y, width, height);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
 	}
 
 	void Init(uint32_t windowWidth, uint32_t windowHeight, uint32_t windowX, uint32_t windowY, const char* windowTitle)
 	{
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+		SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
 		SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
 		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
@@ -175,8 +303,11 @@ public:
 		RetrieveAPIFunctionLocations();
 
 		glEnable(GL_DEPTH_TEST);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
 		glEnable(GL_CULL_FACE);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
 		glFrontFace(GL_CCW);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
 
 		SetViewport(windowWidth, windowHeight, windowX, windowY);
 	}
@@ -209,7 +340,9 @@ public:
 	void Render()
 	{
 		glClearColor(0.f, 0.f, 0.f, 1.f);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		ASSERT(glGetError() == GL_NO_ERROR, "");
 
 		for (int i = 0; i < renderables.size(); i++)
 		{
@@ -217,33 +350,6 @@ public:
 		}
 
 		SDL_GL_SwapWindow(pWindow->handle);
-	}
-};
-
-class Renderable
-{
-public:
-	uint32_t vaoHandle = UINT32_MAX;
-	Buffer* vertexBuffer;
-	Buffer* indexBuffer;
-
-	void SetVertexData(Buffer* vertexBuffer, Buffer* indexBuffer)
-	{
-		if (vaoHandle == UINT32_MAX)
-		{
-			glGenVertexArrays(1, &vaoHandle);
-		}
-
-		glBindVertexArray(vaoHandle);
-		vertexBuffer->Bind(GL_ARRAY_BUFFER);
-		indexBuffer->Bind(GL_ELEMENT_ARRAY_BUFFER);
-		glBindVertexArray(0);
-	}
-
-	void Draw()
-	{
-		glBindVertexArray(vaoHandle);
-		glDrawElements(GL_TRIANGLES, indexBuffer->count, GL_UNSIGNED_INT, 0);
 	}
 };
 
@@ -295,12 +401,16 @@ public:
 	void Run()
 	{
 		Buffer vb;
-		vb.SetData(GL_ARRAY_BUFFER, sizeof(g_quadVertices), sizeof(g_quadVertices) / sizeof(float), g_quadVertices);
+		vb.Init(GL_ARRAY_BUFFER, sizeof(g_quadVertices), sizeof(g_quadVertices) / sizeof(float), g_quadVertices);
 		Buffer ib;
-		ib.SetData(GL_ELEMENT_ARRAY_BUFFER, sizeof(g_quadIndices), sizeof(g_quadIndices) / sizeof(uint32_t), g_quadIndices);
+		ib.Init(GL_ELEMENT_ARRAY_BUFFER, sizeof(g_quadIndices), sizeof(g_quadIndices) / sizeof(uint32_t), g_quadIndices);
 
 		Renderable obj;
 		obj.SetVertexData(&vb, &ib);
+		Shader objShader;
+		objShader.InitAndCompile(g_vertexShaderSource, g_fragmentShaderSource);
+		obj.SetShader(&objShader);
+
 		renderer.AddRenderable(&obj);
 
 		while (isRunning)
