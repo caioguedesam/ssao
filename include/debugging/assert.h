a15d08192148d1b72f2e6357a03dba1bbdabc0d8
Caio Guedes <caioguedesam@gmail.com>
2022-05-28 18:01:57 -0300

    adding format assert

--- include/debugging/assert.h
+++ include/debugging/assert.h
@@ -4,7 +4,16 @@
 #include <cassert>
 #define ASSERT(EXPR, MSG) do {assert(EXPR && MSG);} while(false)
 #define ASSERT_ZERO(EXPR, MSG) do {assert(!EXPR && MSG);} while(false)
+
+#define ASSERT_FORMAT(EXPR, MSG, ...) do {\
+		if(EXPR) {\
+			printf(MSG, __VA_ARGS__);\
+			assert(0);\
+		}\
+	} while(false)
+
 #else
 #define ASSERT(EXPR, MSG) do {} while(false)
 #define ASSERT_ZERO(EXPR, MSG) do {} while(false)
+#define ASSERT_FORMAT(EXPR, MSG, ...) do {} while(false)
 #endif
